7c83b203f5a22b76cb54849874b17c3da0a5222b
Ashley Coles <ashley@acoles.co.uk>
2023-04-27 11:27:03 +0100

    Using the state in the header to display a message

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Footer from "./components/Footer";
 import {useState} from "react";
 
 const App = () => {
+    // Putting the shared state on the nearest common parent
     const [loggedIn, setLoggedIn] = useState(false)
 
     return (
         <>
+            {/*Passing the state down the component tree via props*/}
             <Header loggedIn={loggedIn}/>
             <Content/>
             <Footer/>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,14 @@
+// Here we are using object destructuring to accept
+// the loggedIn prop that was passed to this component
+// by it's parent (App.js)
 const Header = ({loggedIn}) => {
     return (
-        <div>Header</div>
+        <div>
+            <h3>Header</h3>
+            {/*Here we use a ternary to display a message depending on whether or not*/}
+            {/*anyone is logged in*/}
+            {loggedIn ? 'Welcome Back!' : 'Welcome stranger!'}
+        </div>
     )
 }
 
