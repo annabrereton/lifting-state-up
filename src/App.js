2e7310d918cba9121c9063f79a83dc29588b4f51
Ashley Coles <ashley@acoles.co.uk>
2023-04-27 11:22:29 +0100

    Adding state to the nearest parent of the Button and Header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import {useState} from "react";
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <Content />
-      <Footer />
-    </>
-  );
+    const [loggedIn, setLoggedIn] = useState(false)
+
+    return (
+        <>
+            <Header/>
+            <Content/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
