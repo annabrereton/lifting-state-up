f25ea62dd99be42258ebb2c7b0fe7751807b1c26
Ashley Coles <ashley@acoles.co.uk>
2023-04-27 12:03:25 +0100

    Using the toggle function to update the state in App

--- src/components/Footer/Button/index.js
+++ src/components/Footer/Button/index.js
@@ -1,7 +1,10 @@
 // Accepting the loggedIn prop via destructuring
 const Button = ({loggedIn, toggleLoggedIn}) => {
     return (
-        <div>{loggedIn ? 'Logout' : 'Login'}</div>
+        // Calling the toggle function that was passed down
+        // This updates the state on App, which then trickles down the component
+        // tree to update all componants that use the state
+        <button onClick={toggleLoggedIn}>{loggedIn ? 'Logout' : 'Login'}</button>
     )
 }
 
