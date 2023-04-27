d8b64728a1d32e2b90a75831ba00b5efaf1583d4
Ashley Coles <ashley@acoles.co.uk>
2023-04-27 11:17:50 +0100

    React files + basic component structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
